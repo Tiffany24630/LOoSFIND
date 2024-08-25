05b0ed549bf3c642a8093101c8852f41db65b58f
Tiffany24630 <sal24630@uvg.edu.gt>
2024-08-24 21:48:59 -0600

    Update Usuario.java
    Atributos agregados; nombre, correo institucional y contraseña. Métodos agregados; get nombre, set nombre y get correo institucional. Comentarios agregados.

--- Backend/Usuario.java
+++ Backend/Usuario.java
@@ -1,5 +1,33 @@
 package Backend;
 
 public class Usuario {
+  //Atributos (nombre, correo institucional, contraseña, objetos extraviados y objetos encontrados)
+  private String nombre;
+  private String correoInstitucional;
+  private String pasword;
+  
+  //Método get para nombre
+  public String getNombre{
+    return nombre;
+  }
+  
+  //Método set para nombre
+  public void setNombre(String, nombre){
+    this.nombre = nombre;
+  }
 
+  //Método get para correo institucional
+  public String getCorreoInstitucional{
+    return correoInstitucional;
+  }
+  
+  //Método set para correo institucional
+
+
+  //Método get para contraseña
+
+
+  //Método set para contraseña
+  
+  
 }
